559b62615b3805ef633aec74905f3e65c6eb67e8
Ricardo Zavala <zavala.ricardo.m@gmail.com>
2021-05-08 02:34:28 -0700

    second

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,7 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import M from "materialize-css/dist/js/materialize.min.js";
 
 function SearchBar() {
+  document.addEventListener("DOMContentLoaded", function () {
+    var elems = document.querySelectorAll(".sidenav");
+    var instances = M.Sidenav.init(elems);
+  });
+
   return (
     <div>
       <nav
@@ -15,7 +21,31 @@ function SearchBar() {
             {" "}
             Zaxximo
           </Link>
-          <ul id="nav-mobile" className="right">
+          <a
+            href="#"
+            data-target="mobile-demo"
+            class="sidenav-trigger"
+            style={{ float: "right" }}
+          >
+            <i class="material-icons">menu</i>
+          </a>
+
+          <ul id="nav-mobile" className="right hide-on-med-and-down">
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/login">Logout</Link>
+            </li>
+            <li>
+              <Link to="/register">Register</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          </ul>
+
+          <ul class="sidenav" id="mobile-demo">
             <li>
               <Link to="/about">About</Link>
             </li>
